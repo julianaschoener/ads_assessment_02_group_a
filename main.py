8fa580589720f20f1a800130f63f88b0333ec147
juliana schoener <juli.schoener@gmail.com>
2024-04-30 11:12:37 +0200

    Submit assessment Schoener

--- main.py
+++ main.py
@@ -2,8 +2,30 @@
 Simple Calculator
 """
 # Provide your solution here
+def calculate(operand1: int,operand2: int, string_operator: str):
 
+    if string_operator == "+":
+        sum = operand1 + operand2
+        print(str(sum))
+    elif string_operator == "-":
+        sum = operand1 - operand2
+        print(str(sum))
+    elif string_operator == "*":
+        sum = operand1 * operand2
+        print(str(sum))
+    elif string_operator == "/":
+        sum = operand1 / operand2
+        print(str(sum))
+    elif string_operator == "?":
+        sum = operand1 / operand2
+        print("Invalid Operator")
 
+calculate(1,2, "+")
+calculate(1,2, "-")
+calculate(1,2, "*")
+calculate(1,2, "/")
+calculate(0,2, "/")
+calculate(0,2, "?")
 """
 Reverse Word
 """
@@ -13,3 +35,11 @@ Reverse Word
 if __name__ == '__main__':
     print("Test your functions by calling them here. Use different parameter values to test them with different scenarios.")
 
+def reverse_word(word: str) -> str:
+
+    for i in range(len(word)):
+        print(word.capitalize()[-1 - i])
+
+reverse_word("hello")
+reverse_word("WORLD")
+reverse_word("Noel")
